8046d6d369f3216ea41964a2dad34762c1db1d63
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2025-04-25 01:26:05 +0000

    Test

---
+++ projectTest/src/Product.java
@@ -0,0 +1,16 @@
+public class Product {
+    //thông tin mieu ta san pham
+
+    String tenSP;
+    String maSP;
+    int soLuongSP;
+
+
+
+    //Phuong thuc hoat dong cua san pham
+
+    public int ThongkeSoluong(String maSP){
+    
+        return 0;
+    }
+}

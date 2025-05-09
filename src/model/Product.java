d862e6717230415cb17f58b31573026d0dc63324
Nguyen Quoc Thien <quocthien01082005@gmail.com>
2025-05-09 08:37:43 +0700

    Create Product.java

---
+++ src/model/Product.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+    private double price;
+    private int stock;
+
+    public Product(int id, String name, String category, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public double getPrice() { return price; }
+    public int getStock() { return stock; }
+
+    public void setName(String name) { this.name = name; }
+    public void setCategory(String category) { this.category = category; }
+    public void setPrice(double price) { this.price = price; }
+    public void setStock(int stock) { this.stock = stock; }
+
+    @Override
+    public String toString() {
+        return name + " - $" + price;
+    }
+}

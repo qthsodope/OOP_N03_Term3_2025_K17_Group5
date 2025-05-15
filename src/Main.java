1d7f38272f90fe7c41ef1c40c5de2239b097a8fc
Nghiem Xuan Khanh <tpkan12305@gmail.com>
2025-05-15 09:06:27 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import test.UserTest;
-
+import test.ProductTest;
+import test.OrderTest;
 class Main{
     public static void main(String[] args) {
 
         //test User
         UserTest ut = new UserTest();
         ut.test();
+         ProductTest pt = new ProductTest();
+        pt.test();
+        OrderTest ot = new OrderTest();
+        ot.test();
     }
 }

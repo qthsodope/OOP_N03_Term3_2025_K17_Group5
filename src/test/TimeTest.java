7a16cc3f45132e69e937efbaf3bce33610501b5a
Nguyen Quoc Thien <quocthien01082005@gmail.com>
2025-05-16 08:19:54 +0700

    Create TimeTest.java

---
+++ src/test/TimeTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import util.Time;
+
+public class TimeTest {
+    public void test() {
+        Time t = new Time(23, 59, 58);
+        System.out.println("Initial Time: " + t);
+
+        // thay doi time
+        t.setHour(8);
+        t.setMinute(15);
+        t.setSecond(45);
+
+        System.out.println("Updated Time: " + t);
+    }
+}

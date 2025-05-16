a6e1d7804478f4c04e1be6069cd4d3dfa5b0f7b0
Nguyen Quoc Thien <quocthien01082005@gmail.com>
2025-05-16 08:18:20 +0700

    Create Recursion.java

---
+++ src/utils/Recursion.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Recursion {
+    // giai thua 
+    public static long factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    // sum tu 1 -> n
+    public static int sumTo(int n) {
+        if (n <= 1) return 1;
+        return n + sumTo(n - 1);
+    }
+}

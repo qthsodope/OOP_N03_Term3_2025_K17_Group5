f2f898de8e9ec5628729c7055c6bd241ea42b9d5
Nguyen Quoc Thien <quocthien01082005@gmail.com>
2025-05-16 08:20:53 +0700

    Create RecursionTest.java

---
+++ src/test/RecursionTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import util.Recursion;
+
+public class RecursionTest {
+    public void test() {
+        int n = 5;
+        System.out.println(n + "! = " + Recursion.factorial(n));
+        System.out.println("Sum from 1 to " + n + " = " + Recursion.sumTo(n));
+    }
+}

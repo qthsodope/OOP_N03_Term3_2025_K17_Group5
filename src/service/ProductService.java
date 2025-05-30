6b1a4b72236967f09c389e6347e009692f533e5e
Nghiem Xuan Khanh <tpkan12305@gmail.com>
2025-05-30 09:49:51 +0700

    Update ProductService.java

--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -5,14 +5,13 @@ public class ProductService {
     private final ArrayList<Product> productList = new ArrayList<>();
 
     // cập nhật thông tin sản phẩm
-    public void updatedProduct(int id, String name, double price) {
-        for (Product product : productList) {
-            if (product.getId() == id) {
-                product.setName(name);
-                product.setPrice(price);
-                System.out.println("Product with ID " + id + " updated successfully.");
-                break;
+   public boolean updateProductById(int id, Product updatedProduct) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == id) {
+                products.set(i, updatedProduct);
+                return true;
             }
         }
+        return false;
     }
 }

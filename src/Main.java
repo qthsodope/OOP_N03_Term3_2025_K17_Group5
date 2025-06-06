24a3fdd28e342243427943f0861f50752046ef54
Nghiem Xuan Khanh <tpkan12305@gmail.com>
2025-06-06 11:07:28 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-import test.UserTest;
-import test.TimeTest;
-import test.RecursionTest;
+import test.ProductServiceTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== Test User ==");
-        new UserTest().test();
-
-        System.out.println("\n== Test Time ==");
-        new TimeTest().test();
-
-        System.out.println("\n== Test Recursion ==");
-        new RecursionTest().test();
+        ProductServiceTest.run();
     }
 }

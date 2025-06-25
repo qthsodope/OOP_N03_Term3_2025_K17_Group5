4694fd34eab70f921ca618440abc72ee2f5ac9af
bolatao <quocthien01082005@gmail.com>
2025-06-26 02:10:45 +0700

    commit

--- laptopstore/src/main/java/com/laptopstore/LaptopstoreApplication.java
+++ laptopstore/src/main/java/com/laptopstore/LaptopstoreApplication.java
@@ -1,7 +1,12 @@
 package com.laptopstore;
 
+import com.laptopstore.entity.User;
+import com.laptopstore.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LaptopstoreApplication {
@@ -10,4 +15,15 @@ public class LaptopstoreApplication {
 		SpringApplication.run(LaptopstoreApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserRepository repo, PasswordEncoder encoder) {
+		return args -> {
+			if (repo.findByUsername("admin") == null) {
+				repo.save(new User(null, "admin", encoder.encode("123"), "ADMIN"));
+			}
+			if (repo.findByUsername("client") == null) {
+				repo.save(new User(null, "client", encoder.encode("123"), "CLIENT"));
+			}
+		};
+	}
 }

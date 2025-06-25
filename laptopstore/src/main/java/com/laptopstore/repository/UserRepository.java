440d3c10c29d33f0a785b17e1e39790f734ef69e
bolatao <quocthien01082005@gmail.com>
2025-06-26 01:26:41 +0700

    commit

--- laptopstore/src/main/java/com/laptopstore/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.laptopstore.repository;
-
-import com.laptopstore.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}

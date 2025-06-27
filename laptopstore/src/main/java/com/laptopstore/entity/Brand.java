8174f561b492ad86272cd07412e7faeef61d3b6b
bolatao <quocthien01082005@gmail.com>
2025-06-27 18:07:27 +0700

    commit

--- laptopstore/src/main/java/com/laptopstore/entity/Brand.java
+++ laptopstore/src/main/java/com/laptopstore/entity/Brand.java
@@ -1,18 +1,30 @@
 package com.laptopstore.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Brand {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+
+    public Brand() {
+    }
+
+    public Brand(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- laptopstore/src/main/java/com/laptopstore/entity/Laptop.java
+++ laptopstore/src/main/java/com/laptopstore/entity/Laptop.java
@@ -1,24 +1,70 @@
 package com.laptopstore.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Laptop {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String cpu;
     private String ram;
     private long price;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "brand_id")
     private Brand brand;
+
+    public Laptop() {
+    }
+
+    public Laptop(Long id, String name, String cpu, String ram, long price, Brand brand) {
+        this.id = id;
+        this.name = name;
+        this.cpu = cpu;
+        this.ram = ram;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 }

8a5e9ad71b1fda23afe8096121d3536c22214621
bolatao <quocthien01082005@gmail.com>
2025-06-27 19:34:01 +0700

    commit

--- laptopstore/src/main/java/com/laptopstore/util/JdbcUtil.java
+++ laptopstore/src/main/java/com/laptopstore/util/JdbcUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class JdbcUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/laptopdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "12032005";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
